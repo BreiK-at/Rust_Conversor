1429b744d6dbc75c1ecf7c9527819a89ac8503d9
MAICOL_ESTIVEN_VASCO_CARDENAS <michael.vasco01@uceva.edu.co>
2025-11-03 00:52:19 -0500

    cambio en la estructura + agregado de funciones

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,56 @@ fn main() {
 
     println!("=== CONVERSOR DE TEMPERATURA ===");
 
-    println!("Ingresa el valor en grados Celcius");
+    loop {
+
+        println!("\nElige la conversión:");
+        println!("1. Celsius a Fahrenheit");
+        println!("2. Fahrenheit a Celsius");
+        println!("3. Salir");
+
+        let mut choice = String::new();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+
+        match choice.trim().parse() {
+            Ok(1) => convertir_c_A_f(),
+            Ok(2) => convertir_f_A_c(),
+            Ok(3) => {
+                println!("hasta luego");
+                break;
+            }
+            _ => println!("por favor ingresa una opcion valida"),
+        }
+
+    }
+
+}
+
+fn convertir_c_A_f() {
+
+    println!("Ingrese el valor en celsius");
+    let celsius = obtener_numero();
+    let fahrenheit = celsius * 1.8 + 32.0;
+    println!("{}°C = {:.1}°F", celsius, fahrenheit);
+
+}
+
+fn convertir_f_A_c() {
+    println!("Ingresa grados Fahrenheit:");
+    let fahrenheit = obtener_numero();
+    let celsius = (fahrenheit - 32.0) / 1.8;
+    println!("{}°F = {:.1}°C", fahrenheit, celsius);
+}
+
+fn obtener_numero() -> f64 {
 
     let mut input = String::new();
 
     io::stdin()
-
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let celsius: f64 =
-
-        input.trim()
-        .parse()
-        .expect("Please type a number!");
-
-    let fahrenheit = celsius * 1.8 + 32.0;
-
-    println!("{}°C = {}°F", celsius, fahrenheit);
-
+    input.trim().parse().expect("Please type a number!")
 }
